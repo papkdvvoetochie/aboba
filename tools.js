c28e2d2562e2343bf5d89bdcc21973ea61154a9c
papkdvvoetochie <150657045+papkdvvoetochie@users.noreply.github.com>
2026-03-03 20:51:19 +0300

    Update and rename tools.ts to tools.js

--- tools.js
+++ tools.js
@@ -1,13 +1,11 @@
-export async function throwLog(str: string): Promise<void> {
+export async function throwLog(str) {
     const now = new Date().toLocaleString();
     console.log(`${now} INFO - ${str}`);
 }
-
-export async function throwErr(err: string): Promise<void> {
+export async function throwErr(err) {
     const now = new Date().toLocaleString();
     console.error(`${now} ERROR - ${err}`);
 }
-
-export async function returnName(type: string): Promise<string> {
+export async function returnName(type) {
     return "temp-" + Math.floor(Math.random() * 99999) + type;
 }
